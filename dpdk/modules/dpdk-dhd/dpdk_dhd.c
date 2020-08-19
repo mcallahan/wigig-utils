839353b97c3c83c92af67041e8d5f1b3e168081a
Lior David <liord@codeaurora.org>
2020-08-18 17:22:22 -0700

    dpdk-dhd: add license headers to the dpdk-dhd kernel module
    Add GPL/BSD license headers to the dpdk-dhd linux
    kernel module source files.  Use the SPDX format already in
    use by the rest of the DPDK upstream code.
    
    Original author: Michael Callahan <michaelcallahan@fb.com>
    
    Change-Id: I9c19237d3614d56ae18b2fd485099e7275920a49

--- dpdk/modules/dpdk-dhd/dpdk_dhd.c
+++ dpdk/modules/dpdk-dhd/dpdk_dhd.c
@@ -1,3 +1,6 @@
+/* SPDX-License-Identifier: GPL-2.0-only OR BSD-3-Clause */
+/* Copyright (c) 2020, Facebook, Inc. All rights reserved. */
+
 #include <linux/version.h>
 #include <linux/module.h>
 #include <linux/miscdevice.h>
--- dpdk/modules/dpdk-dhd/dpdk_dhd.h
+++ dpdk/modules/dpdk-dhd/dpdk_dhd.h
@@ -1,3 +1,6 @@
+/* SPDX-License-Identifier: GPL-2.0-only OR BSD-3-Clause */
+/* Copyright (c) 2020, Facebook, Inc. All rights reserved. */
+
 #ifndef DPDK_DHD_
 #define DPDK_DHD_
 
